e1b5d714d10edeadb297180e51160f8c121cacb8
Basile Grosjean <bgrosjea@k0r1p5.42mulhouse.fr>
2024-01-14 18:02:54 +0100

    set

--- main.c
+++ main.c
@@ -6,14 +6,17 @@
 /*   By: bgrosjea <bgrosjea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 14:04:02 by bgrosjea          #+#    #+#             */
-/*   Updated: 2024/01/14 14:57:30 by bgrosjea         ###   ########.fr       */
+/*   Updated: 2024/01/14 17:59:07 by bgrosjea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **env)
 {
+	char	**path;
+
+	path = find_path(env);
 	if (argc != 5)
 		return (write(2, "Arguments Error\n", 17));
 	return (0);
