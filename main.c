f8f6550339869c31bacbba9d74aa6ba765b9bd63
Basile Grosjean <bgrosjea@k0r1p3.42mulhouse.fr>
2024-01-17 14:57:52 +0100

    brouillon

--- main.c
+++ main.c
@@ -6,18 +6,61 @@
 /*   By: bgrosjea <bgrosjea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 14:04:02 by bgrosjea          #+#    #+#             */
-/*   Updated: 2024/01/14 17:59:07 by bgrosjea         ###   ########.fr       */
+/*   Updated: 2024/01/16 18:36:24 by bgrosjea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+#include "stdio.h"
 
 int	main(int argc, char **argv, char **env)
 {
-	char	**path;
+	int i;
+	int boul;
+	char **path;
 
-	path = find_path(env);
-	if (argc != 5)
-		return (write(2, "Arguments Error\n", 17));
-	return (0);
+	i = 0;
+	boul = 1;
+	while (0 != ft_strncmp(env[i], "PATH=", 5))
+		i++;
+	path = ft_split(env[i], ':');
+	i = 1;
+	while (path[i])
+	{
+		if (access(path[i], F_OK) == 0)
+		{
+			if (access(path[i], X_OK) == 0)
+			{
+				boul = 0;
+				break ;
+			}
+		}
+	}
+	(void) argc;
+	(void) argv;
+	if (boul = 1)
+
+	else
+		exit ((perror("Error"), 1));
+	i = 0;
+	// gestion d erreur de proc enfant 
+
+	int wstatus;
+	int scode;
+	
+	wait(&wstatus);
+	if (WIFEXITED(wstatus))
+			if (0 == WEXITSTATUS(wstatus));
+				exit (1);
+	while (env[i])
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}
 }
+
+	// char	**path;
+
+	// path = find_path(env);
+	// if (argc != 5)
+	// 	return (write(2, "Arguments Error\n", 17));
